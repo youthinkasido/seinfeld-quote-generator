fe6a3f8d9cba94dfc89f72a7cd48dc2b06453adf
Arshia Khoshnood <youthinkasido@gmail.com>
2019-10-13 15:16:40 -0400

    Require module testing

---
+++ Desktop/sqg/seinfeld-quote-generator/app.js
@@ -0,0 +1,4 @@
+var app = require('./test')
+
+console.log(app.counter([1,2,3]))
+console.log(app.add(app.pi,4))
---
+++ Desktop/sqg/seinfeld-quote-generator/test.js
@@ -0,0 +1,17 @@
+var add = function(a,b){
+    return `${a} + ${b} equals ${a+b}`
+}
+
+var counter = function(arr){
+    return `the length is ${arr.length}`
+}
+
+var pi= 3.14
+
+module.exports = {
+    pi:pi,
+    add:add,
+    counter:counter
+}
+
+
