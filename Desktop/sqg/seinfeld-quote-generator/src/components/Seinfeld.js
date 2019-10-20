b0ddd6cc037643e31418283f66b32c4191b688ec
Arshia Khoshnood <youthinkasido@gmail.com>
2019-10-20 19:03:27 -0400

    Render more quote data

--- Desktop/sqg/seinfeld-quote-generator/src/components/Seinfeld.js
+++ Desktop/sqg/seinfeld-quote-generator/src/components/Seinfeld.js
@@ -1,54 +1,59 @@
-import React, { Component } from 'react'
-import $ from 'jquery'
-import SeinfeldQuote from './SeinfeldQuote'
+import React, {Component} from 'react';
+import $ from 'jquery';
+import SeinfeldQuote from './SeinfeldQuote';
+import './Seinfeld.css';
 export default class Seinfeld extends Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            quotes:null
-        }
-    }
+  constructor (props) {
+    super (props);
+    this.state = {
+      quotes: null,
+    };
+  }
 
-    componentDidMount(){
-        $.ajax({
-            url:"https://seinfeld-quotes.herokuapp.com/quotes",
-            dataType:"JSON",
-            method: "GET"
-        }).then((res) =>{
-           this.setState({quotes:Object.values(res.quotes)})
-        })
-    }
+  componentDidMount () {
+    $.ajax ({
+      url: 'https://seinfeld-quotes.herokuapp.com/quotes',
+      dataType: 'JSON',
+      method: 'GET',
+    }).then (res => {
+      this.setState ({quotes: Object.values (res.quotes)});
+    });
+  }
 
-    render() {
-        let quotes;
-        
-        
-        if (this.state.quotes){
-            console.log(this.state.quotes)
-             quotes = this.state.quotes.map(quote=>{
-                return (
-                <ul>
-                    <li>{quote.quote}</li>
-                    <li>{quote.author}</li>
-                </ul>
-                )
-            })
-        }else{
-            return null;
-        }
-        console.log(quotes)
+  render () {
+    let quotes;
+
+    if (this.state.quotes) {
+      console.log (this.state.quotes);
+      quotes = this.state.quotes.map (quote => {
         return (
-        <div className="quote-container">
-            <div className="quotes">
-
-            {quotes}
-            </div>
-            
-            </div>
-        )
-        
+          <div className="quote">
+            <ul className="quote-content">
+              <li>
+                <img
+                  className="thumb-nail"
+                  src="https://www.tvinsider.com/wp-content/uploads/2015/07/Seinfeld-Kramer.jpg"
+                />
+              </li>
+              <li>Author:{quote.author}</li>
+              <li>Quote:{quote.quote}</li>
+              <li>Season:{quote.season}</li>
+              <li>Episode:{quote.episode}</li>
+            </ul>
+          </div>
+        );
+      });
+    } else {
+      return null;
     }
+    console.log (quotes);
+    return (
+      <div className="quote-container">
+        <div className="quotes">
+          {quotes}
+        </div>
+
+      </div>
+    );
+  }
 }
-
-
-
