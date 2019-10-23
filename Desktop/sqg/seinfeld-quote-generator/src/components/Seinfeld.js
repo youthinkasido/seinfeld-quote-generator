4911625e7df0b029c8fab6c966a0633399db1e65
Arshia Khoshnood <youthinkasido@gmail.com>
2019-10-22 21:37:32 -0400

    Remove images

--- Desktop/sqg/seinfeld-quote-generator/src/components/Seinfeld.js
+++ Desktop/sqg/seinfeld-quote-generator/src/components/Seinfeld.js
@@ -21,32 +21,32 @@ export default class Seinfeld extends Component {
   }
 
   render () {
+    let filteredQuotes;
     let quotes;
 
     if (this.state.quotes) {
-      quotes = this.state.quotes.map (quote => {
+      filteredQuotes = this.state.quotes.filter (
+        quote => quote.quote.length < 50
+      );
+      quotes = filteredQuotes.map (quote => {
         return (
-          <div className="quote">
+          <div className="quotes">
             <ul className="quote-content">
-              <li>
-                {quote.quote.length < 100
-                  ? <img
-                      className="thumb-nail"
-                      src="https://www.tvinsider.com/wp-content/uploads/2015/07/Seinfeld-Kramer.jpg"
-                    />
-                  : null}
+              <div className="quote-text-container">
+                <ul>
+                  <li>
+                    Author:{quote.author}
+                  </li>
+                  <li className="quote-text">
+                    "{quote.quote}"{' '}
+                  </li>
+                  Season: {quote.season}
 
-              </li>
-              <li>
-                {quote.quote.length < 100 ? `Author:` + quote.author : null}
-              </li>
-              <li className="quote-text">
-                {quote.quote.length < 100 ? quote.quote : null}
-              </li>
-              {quote.quote.length < 100 ? `Season:` + quote.season : null}
-              <li>
-                {quote.quote.length < 100 ? `Episode:` + quote.episode : null}
-              </li>
+                  <li>
+                    Episode: {quote.episode}
+                  </li>
+                </ul>
+              </div>
             </ul>
           </div>
         );
