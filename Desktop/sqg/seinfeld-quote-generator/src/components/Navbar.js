7303cae29d11276c4054ee5f7e1b1c73b6631aa0
Arshia Khoshnood <youthinkasido@gmail.com>
2019-10-23 13:44:49 -0400

    Add fixed nav bar

--- Desktop/sqg/seinfeld-quote-generator/src/components/Navbar.js
+++ Desktop/sqg/seinfeld-quote-generator/src/components/Navbar.js
@@ -5,9 +5,15 @@ export default class Navbar extends Component {
     return (
       <div className="nav-bar-container">
         <ul className="nav-bar">
-          <li className="nav-item">Quotes</li>
-          <li className="nav-item">Press</li>
-          <li className="nav-item">About</li>
+
+          <img src="http://tny.im/jNM/" />
+          <p>
+            {/* Seinfeld is an American television sitcom that originally aired on NBC from July 5, 1989, to May 14, 1998. It lasted nine seasons, and is now in syndication. It was created by Larry David and Jerry Seinfeld, the latter starring as a fictionalized version... */}
+          </p>
+
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Quotes</a></li>
+          <li><a href="#">Purchase</a></li>
         </ul>
       </div>
     );
