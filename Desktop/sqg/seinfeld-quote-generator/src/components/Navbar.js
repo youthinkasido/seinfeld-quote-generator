cb9fa5cc46c65038c5d4cdd5235ea9f0afc600b8
Arshia Khoshnood <youthinkasido@gmail.com>
2019-10-24 21:52:12 -0400

    Style update

--- Desktop/sqg/seinfeld-quote-generator/src/components/Navbar.js
+++ Desktop/sqg/seinfeld-quote-generator/src/components/Navbar.js
@@ -10,10 +10,23 @@ export default class Navbar extends Component {
           <p>
             {/* Seinfeld is an American television sitcom that originally aired on NBC from July 5, 1989, to May 14, 1998. It lasted nine seasons, and is now in syndication. It was created by Larry David and Jerry Seinfeld, the latter starring as a fictionalized version... */}
           </p>
+          <li>
 
-          <li><a href="#">Home</a></li>
+            <p>
+              Seinfeld is an American television sitcom that
+              originally aired on NBC from July 5, 1989, to May 14, 1998.
+              It lasted nine seasons, and is now in syndication.
+              It was created by Larry David and Jerry Seinfeld,
+              the latter starring as a fictionalized version...
+            </p>
+            <a href="https://twitter.com/SeinfeldTV">
+              <img src="https://cfcdnpull-creativefreedoml.netdna-ssl.com/wp-content/uploads/2017/06/Twitter-logo-2012.png" />
+            </a>
+          </li>
+
+          {/* <li><a href="#">Home</a></li>
           <li><a href="#">Quotes</a></li>
-          <li><a href="#">Purchase</a></li>
+          <li><a href="#">Purchase</a></li> */}
         </ul>
       </div>
     );
